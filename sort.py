ec5945918eccb30ca14ead00e692dcffd610626b
jiayiliu2016 <jiayliu_sera@163.com>
2017-05-24 21:59:48 +0800

    common commit

--- sort.py
+++ sort.py
@@ -6,7 +6,7 @@
 # 大循环： 从第一个元素开始，到最后一个元素
 # 小循环： 对所有该小循环中的元素，两两比较元素大小，如果有根据大小，交换两个元素的位置
 
-def bubblesort(arr):
+def bubbleSort(arr):
 	for i in range (0,len(arr)):
 		for j in range(i+1,len(arr)):
 			if arr[i]>arr[j]:
@@ -21,7 +21,7 @@ def bubblesort(arr):
 # 从arr[0]~arr[len(arr)-1]中找出最小的元素及它的的标志位min，将arr[min]与arr[0]交换
 # 从arr[1]~arr[len(arr)-1]中找出最小的元素及它的的标志位min，将arr[min]与arr[1]交换
 
-def selectionsort(arr):
+def selectionSort(arr):
 	for i in range (0,len(arr)):
 		min = i
 		for j in range (i+1,len(arr)):
@@ -37,7 +37,7 @@ def selectionsort(arr):
 # 对整个序列，从前往后扫描  取待排序序列。 把最后一个元素作为寻找插入位的元素（待排序元素）；
 # 对取出的待排序序列，从后往前扫描  寻找插入位置。 当key更小时往前插一位()
 
-def insertionsort(arr):
+def insertionSort(arr):
 	for i in range(1,len(arr)):
 		j=i-1
 		key=arr[i]
@@ -56,7 +56,7 @@ def insertionsort(arr):
 # 每次 gap=gap/3， 直到 gap=math.floor(gap/3)=0
 
 import math
-def shellsort(arr):
+def shellSort(arr):
 	import math
 	gap=1
 	while(gap<len(arr)/3):
@@ -96,12 +96,12 @@ def merge(left,right):
 				temp.append(x)
 		return temp
 
-def mergesort(arr):
+def mergeSort(arr):
 	if len(arr)<2:
 		return arr
 	middle=len(arr)/2
-	left=mergesort(arr[:middle])
-	right=mergesort(arr[middle:])
+	left=mergeSort(arr[:middle])
+	right=mergeSort(arr[middle:])
 	return merge(left,right)
 
 
@@ -110,7 +110,7 @@ def mergesort(arr):
 # 取出基准元素，比它小的元素放 low[],大的放high[], 并拿到基准元素的标志位key_index（arr[key_index]=key）
 # 分别对low[]， high[] 进行快速排序
 
-def sub_sort(array,low,high):
+def subSort(array,low,high):
     key = array[low]
     while low < high:
         while low < high and array[high] >= key:
@@ -122,41 +122,45 @@ def sub_sort(array,low,high):
     array[low] = key
     return low
 
-def quick_sort(array,low,high):
+def quickSort(array,low,high):
      if low < high:
-        key_index = sub_sort(array,low,high)
-        quick_sort(array,low,key_index)
-        quick_sort(array,key_index+1,high)
+        key_index = subSort(array,low,high)
+        quickSort(array,low,key_index)
+        quickSort(array,key_index+1,high)
 
 
 
-# heap sort
-# 直接选择排序的改进版。 
-# 1、建堆：大根堆；2、调堆：A[0]与A[lenth-1]互换，再调整对为大根堆
-# 从最后一个非叶子节点i开始，将以i为根节点的二叉树调整为堆 （目标：所有的子树都为堆的完全二叉树）
-def adjust_heap(lists, i, size):
-    lchild = 2 * i + 1
-    rchild = 2 * i + 2
-    max = i
-    if i < size / 2:
-        if lchild < size and lists[lchild] > lists[max]:
-            max = lchild
-        if rchild < size and lists[rchild] > lists[max]:
-            max = rchild
-        if max != i:
-            lists[max], lists[i] = lists[i], lists[max]
-            adjust_heap(lists, max, size)
- 
-def build_heap(lists, size):
-    for i in range(0, (size/2))[::-1]:
-        adjust_heap(lists, i, size)
- 
-def heap_sort(lists):
-    size = len(lists)
-    build_heap(lists, size)
-    for i in range(0, size)[::-1]:
-        lists[0], lists[i] = lists[i], lists[0]
-        adjust_heap(lists, 0, i)
+# heapsort
+# 直接选择排序的改进版(选出合适的元素索引，再做一次交换)。
+# len(A)=n, A[i]出节点： 双亲节点为 A[(i-1)/2]
+# 			  		  左孩子节点为 A[2i+1]， 若2i+1>n时，A[i]无左孩子节点，为叶子
+# 			  		  右孩子节点为 A[2i+2]， 若2i+2>n时，A[i]无右孩子节点
+# 从最后一个非叶子节点A[i]开始（即A[n/2-1]），将以A[i]为根节点的二叉树调整为堆 （目标：所有的子树都为堆的完全二叉树）
+# 1、建堆：大根堆（从A[n/2-1]开始，目标是A[0]最大）；2、调堆：A[0]与A[lenth-1]互换位置，输出末尾节点，再对(n-2)的树调整，重新组成大根堆
+
+def adjustHeap(arr, i, arrLen):
+	max=i
+	left=2*i+1
+	right=2*i+2
+	if i<arrLen/2:
+		if left< arrLen and arr[left]>arr[max]:
+			max=left
+		if right<arrLen and arr[right]>arr[max]:
+			max=right
+		if i!=max:
+			arr[i]=arr[max]
+			adjustHeap(arr, max, arrLen)
+
+def buildHeap(arr,arrLen):
+	for i in range (0,arrLen/2)[::-1]:
+		adjustHeap(arr, i, arrLen)
+
+def heapSort(arr):
+	arrLen=len(arr)
+	buildHeap(arr,arrLen)
+	for i in range (0,arrLen)[::-1]:
+		arr[0],arr[i]=arr[i],arr[0]
+		adjustHeap(arr, 0, i)
 
     # array = [1, 2, 5, 3, 6, 8, 4]
 	# >>> array[::2]
@@ -174,9 +178,6 @@ def heap_sort(lists):
 	# [4, 6, 5, 1]
 
 if __name__ == '__main__':
-    arr = raw_input()
+    arr = rawInput()
     # sort(arr)
-    print merge_sort(arr)
-
-
-
+    print mergSort(arr)
